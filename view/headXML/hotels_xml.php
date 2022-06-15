e96ac99fe9c359673f25ed9251757f3360236499
icaro2222 <icarodias2222@gmail.com>
2022-06-15 20:23:04 -0300

    .

---
+++ view/headXML/hotels_xml.php
@@ -0,0 +1,42 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once('../../app/controller/Hotel.php');
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link rel="stylesheet" href="../../public/css/style.css">
+<head>
+<meta charset="utf-8">
+
+</head>
+<body>
+<h1>Hotels</h1>
+<?php
+    $link = "/var/www/html/Icaro/document/avaliacao-candidatos-main/database/hotels.xml"; 
+    //link do arquivo xml
+    $xml = simplexml_load_file($link); 
+    //carrega o arquivo XML e retornando um Array
+
+    $hotel = new Hotel;
+
+    foreach($xml->children() as $Hotel) {
+        $hotel->setId($Hotel['id']);
+        $hotel->setNome($Hotel->Name);
+
+        // echo $hotel->getNome();
+        // echo "<br>Hotel ". $hotel->getNome(). " inserido no banco com sucesso";
+
+        if($hotel->insert()){
+            echo "<br><br>Hotel ". $hotel->getNome(). " inserido no banco com sucesso";
+        }
+    }
+
+?>
+</body>
+</html>
---
+++ view/headXML/rooms_xml.php
@@ -0,0 +1,39 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once('../../app/controller/Room.php');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<link rel="stylesheet" href="../../public/css/style.css">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+
+</head>
+<body>
+<h1>Rooms</h1>
+<?php
+    $link = "/var/www/html/Icaro/document/avaliacao-candidatos-main/database/rooms.xml"; 
+    //link do arquivo xml
+    $xml = simplexml_load_file($link); 
+    //carrega o arquivo XML e retornando um Array
+
+    $room = new Room;
+    
+    foreach($xml->children() as $Rooms) {
+
+        $room->setId($Rooms['id']);
+        $room->setNome($Rooms->Name);
+        $room->setFk($Rooms['hotelCode']);
+        
+        if($room->insert()){
+            echo "<br><br>Room ". $room->getNome(). " inserido no banco com sucesso";
+        }
+    }
+?>
+</body>
+</html>
